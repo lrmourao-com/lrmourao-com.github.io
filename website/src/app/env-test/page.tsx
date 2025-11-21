303e48db38bc2c5445e223ebe5273f1dbd24043d
Pedro Roque <pmm26@protonmail.com>
2025-11-21 00:04:55 +0000

    save

--- website/src/app/env-test/page.tsx
+++ website/src/app/env-test/page.tsx
@@ -3,6 +3,7 @@ export default function EnvTest() {
     <div>
       <h1>Env Test</h1>
       <p>{process.env.NEXT_PUBLIC_ENV}</p>
+      {typeof window !== 'undefined' && process.env.NEXT_PUBLIC_ENV === 'production' ? true : false}
     </div>
   );
 }
