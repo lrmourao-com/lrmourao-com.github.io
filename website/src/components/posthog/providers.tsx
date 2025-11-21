854c1ea3d8e79f45dbc72036ce6d76327a6aa65d
Pedro Roque <pmm26@protonmail.com>
2025-11-21 00:22:42 +0000

    variables

--- website/src/components/posthog/providers.tsx
+++ website/src/components/posthog/providers.tsx
@@ -9,7 +9,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
     // Only initialize PostHog in production
     if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_ENV === 'production') {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-        api_host: 'https://server.lrmourao.fazerweb.com/api/posthog',
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
         ui_host: 'https://eu.posthog.com',
         person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
         capture_pageview: false, // Disable automatic pageview capture, as we capture manually
